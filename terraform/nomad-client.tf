11b1a5e80b6fec365de5228f0b26a1bf8c54294d
Vaibhav Jaiswal <vaibhavjaiswal871@gmail.com>
2025-09-19 00:10:08 +0530

    changes in script

--- terraform/nomad-client.tf
+++ terraform/nomad-client.tf
@@ -7,9 +7,7 @@ resource "aws_instance" "nomad_client" {
   vpc_security_group_ids      = [aws_security_group.nomad_sg.id]
   associate_public_ip_address = true
 
-  user_data = templatefile("${path.module}/../cloud-init/nomad-client.sh", {
-    NOMAD_SERVER_IP = aws_instance.nomad_server.private_ip
-  })
+  user_data = templatefile("${path.module}/../cloud-init/nomad-client.sh")
 
 
   depends_on = [aws_instance.nomad_server]
--- terraform/nomad-server.tf
+++ terraform/nomad-server.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "nomad_server" {
   vpc_security_group_ids      = [aws_security_group.nomad_sg.id]
   associate_public_ip_address = false
 
-  user_data = templatefile("${path.module}/../cloud-init/nomad-server.sh", {})
+  user_data = templatefile("${path.module}/../cloud-init/nomad-server.sh")
 
   tags = {
     Name = "nomad-server"
