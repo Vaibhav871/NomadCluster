2e969aaaf7e1a652c618d1a0e894a386cb92c874
vaibhav <vaibhavjaiswal871@gmail.com>
2025-09-25 17:16:45 +0530

    test

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -9,7 +9,6 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-  
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
